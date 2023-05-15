35fd25ea1e411a94482ff586304edf32369d470c
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-05-16 00:04:56 +0300

    customization

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { styles } from '../styles';
 import { navLinks } from '../constants';
-import { shaq, bwmap, worldmap } from '../assets';
+import { shaq, kevin, bwmap, worldmap } from '../assets';
 
 const Hero = () => {
   return (
@@ -37,17 +37,17 @@ const Hero = () => {
           <div>
             <h1
               className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
-              Hi, I'm{' '}
+              Hi, I&apos;m{' '}
               <span
                 className="sm:text-battleGray sm:text-[90px] 
                 text-eerieBlack text-[50px] font-mova
                 font-extrabold uppercase">
-                Shaquille
+                Kevin
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
-              Lorem ipsum dolor sit amet. <br className="sm:block hidden" />
-              consectetur adipisicing elit deleniti, voluptas.
+              A fullstack software engineer. <br className="sm:block hidden" />
+              I specialize in Ruby on Rails and the MERN stack.
             </p>
           </div>
           <div
@@ -86,7 +86,7 @@ const Hero = () => {
             className="absolute bottom-0 ml-[50vw] 
             lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
             sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
-            src={shaq}
+            src={kevin}
             alt="shaquille"
           />
         </div>
